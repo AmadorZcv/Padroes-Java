2bfffe4fee05c941ff6e83093b70427febb5a408
AmadorZcv <lucasamadorzcv@gmail.com>
2018-12-18 05:32:30 -0300

    Implementação do FactoryMethod

--- Padroes-Java/src/FactoryMethod/FactoryMethod.java
+++ Padroes-Java/src/FactoryMethod/FactoryMethod.java
@@ -10,5 +10,14 @@ package FactoryMethod;
  * @author Amador
  */
 public class FactoryMethod {
-    
+
+    public static void main(String[] args) {
+        EmissorCreator factory = new EmissorCreator();
+        Emissor e1 = factory.create(EmissorCreator.SMS);
+        e1.envia("Você acabou de realizar compra de R$100,00");
+        Emissor e2 = factory.create(EmissorCreator.EMAIL);
+        e2.envia("Você acabou de realizar compra de R$100,00");
+        Emissor e3 = factory.create(EmissorCreator.JMS);
+        e3.envia("Você acabou de realizar compra de R$100,00");
+    }
 }
