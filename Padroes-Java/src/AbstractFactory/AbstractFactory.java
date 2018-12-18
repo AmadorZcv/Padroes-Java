9cd746c4321220fc6ade36ce619d35c27e3f7e0c
AmadorZcv <lucasamadorzcv@gmail.com>
2018-12-18 06:01:09 -0300

    Implementação do AbstractFactory

--- Padroes-Java/src/AbstractFactory/AbstractFactory.java
+++ Padroes-Java/src/AbstractFactory/AbstractFactory.java
@@ -10,5 +10,17 @@ package AbstractFactory;
  * @author Amador
  */
 public class AbstractFactory {
-    
+
+    public static void main(String[] args) {
+//Loja com máquina da REDE
+        FabricaAbstrata maquina1 = new Rede();
+        maquina1.criarVisa().executar(30);
+        maquina1.criarMasterCard().executar(150);
+        maquina1.criarElo().executar(50);
+//Loja com máquina da CIELO
+        FabricaAbstrata maquina2 = new Cielo();
+        maquina2.criarVisa().executar(35);
+        maquina2.criarMasterCard().executar(155);
+        maquina2.criarElo().executar(55);
+    }
 }
